48289c0846d2749ffb158046092f8e81da030e8b
OussDataDevOps <zaid.oussama@gmail.com>
2023-12-09 20:16:34 +0100

    add

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "main" {
 
   tag {
     key                 = "Name"
-    value               = "${var.tag_asg}-ASG-Instance-${count.index}"
+    value               = var.tag_asg
     propagate_at_launch = true
   }
 }
