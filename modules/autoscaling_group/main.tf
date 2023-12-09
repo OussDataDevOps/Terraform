a0866e2211797ce5d913aaccffbf0e385ed27e17
OussDataDevOps <zaid.oussama@gmail.com>
2023-12-09 20:21:57 +0100

    add mario

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -8,6 +8,21 @@ resource "aws_launch_configuration" "main" {
   lifecycle {
     create_before_destroy = true
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+              apt-get update -y
+              apt-get install -y docker-ce
+              systemctl start docker
+              systemctl enable docker
+              docker pull kaminskypavel/mario
+              docker run -d -p 80:8080 kaminskypavel/mario
+              EOF
+
 }
 
 resource "aws_autoscaling_group" "main" {
